e1c2718943d7bb10e5feca552af1f860cc956eb6
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2022-05-10 23:13:07 +0500

    Очередные правки

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ const profileEdit = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+
 profileEdit.addEventListener('click', function() {
-   if (popup.classList.contains('popup_open') !== true) {
-      popup.classList.add('popup_open');
+   nameInput.value = profileName.textContent;
+   jobInput.value = profileDescription.textContent;
+   if (popup.classList.contains('popup_opened') !== true) {
+      popup.classList.add('popup_opened');
    }
 });
 
 closeButton.addEventListener('click', function() {
-   if (popup.classList.contains('popup_open') !== false) {
-      popup.classList.remove('popup_open');
+   if (popup.classList.contains('popup_opened') !== false) {
+      popup.classList.remove('popup_opened');
    }
 })
 
+let formElement = document.querySelector('.popup__form');
 
+function formSubmitHandler (evt) {
+   evt.preventDefault();
 
+   profileName.textContent = nameInput.value;
+   profileDescription.textContent = jobInput.value;
+}
 
-
-// const likeButton = document.querySelectorAll('.element__like-button');
-// likeButton.addEventListener('click', function() {
-   // for (let i = 0; i <= likeButton.length-1; i ++) {
-   //    likeButton[i].classList.toggle('element__like-active');
-   // }
-//    alert('да да я');
-// });
-
-//    likeButton.classList.toggle('element__like-active');
-//    alert('да да я');
+formElement.addEventListener('submit', formSubmitHandler); 
