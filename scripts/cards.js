778d24d30447a16cc2abad509d776e6317eec7a7
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2022-05-28 16:19:50 +0500

    fix: js

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+   {
+      name: 'Карелия',
+      link: 'https://images.unsplash.com/photo-1525301472244-57e18fb6f028?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764'
+   },
+   {
+      name: 'Северная Осетия',
+      link: 'https://images.unsplash.com/photo-1612719734820-81784b7e6573?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687'
+   },
+   {
+      name: 'Онежское озеро',
+      link: 'https://images.unsplash.com/photo-1543699936-c901ddbf0c05?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686'
+   },
+   {
+      name: 'Красноярск',
+      link: 'https://images.unsplash.com/photo-1628872247398-888e0808c130?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627'
+   },
+   {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+      name: 'Крым',
+      link: 'https://images.unsplash.com/photo-1565342403875-07a8dc5ed13c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764'
+    }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,4 @@
-const initialCards = [
-   {
-      name: 'Карелия',
-      link: 'https://images.unsplash.com/photo-1525301472244-57e18fb6f028?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764'
-   },
-   {
-      name: 'Северная Осетия',
-      link: 'https://images.unsplash.com/photo-1612719734820-81784b7e6573?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687'
-   },
-   {
-      name: 'Онежское озеро',
-      link: 'https://images.unsplash.com/photo-1543699936-c901ddbf0c05?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686'
-   },
-   {
-      name: 'Красноярск',
-      link: 'https://images.unsplash.com/photo-1628872247398-888e0808c130?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627'
-   },
-   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-   },
-   {
-      name: 'Крым',
-      link: 'https://images.unsplash.com/photo-1565342403875-07a8dc5ed13c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764'
-    }
-]; 
-
+const templateElement = document.querySelector('.element-template').content;
 
 const profileEditButton = document.querySelector('.profile__edit-button');
 const closePopupButtons = document.querySelectorAll('.popup__close-button');
@@ -52,7 +26,8 @@ function formSubmitHandler(evt) {
 
    profileName.textContent = nameInput.value;
    profileDescription.textContent = jobInput.value;
-   editPopup.classList.remove('popup_opened');
+
+   closePopup(editPopup);
 };
 
 formElement.addEventListener('submit', formSubmitHandler); 
@@ -67,7 +42,7 @@ function renderList(data) {
 // Лайк элемента
 function likeListItem(evt) {
    const buttonElement = evt.target;
-   buttonElement.closest('.element__like-button').classList.toggle('element__like-button_active');
+   buttonElement.classList.toggle('element__like-button_active');
 };
 
 
@@ -86,23 +61,28 @@ function openPhotoPopup(evt) {
    const popupCaption = document.querySelector('.popup__caption');
    const elementImage = evt.target.closest('.element__image');
    popupImage.src = elementImage.src;
+   popupImage.alt = elementImage.alt;
    popupCaption.textContent = elementImage.alt;
    openPopup(photoPopup);
 };
 
 
 // Картинки "ИЗ КОРОБКИ"
-function renderItem(text) {
-   const templateElement = document.querySelector('.element-template').content;
+function createCard(text) {
    const listElement = templateElement.querySelector('.element').cloneNode(true);
    const nameElement = listElement.querySelector('.element__title');
    const elementImage = listElement.querySelector('.element__image');
    nameElement.textContent = text.name;
    elementImage.src = text.link;
    elementImage.alt = text.name;
-   
+
    subscribeToEvents(listElement);
-   cardsList.prepend(listElement);
+   return listElement;
+};
+
+
+function renderItem(cardData) {
+   cardsList.prepend(createCard(cardData));
 };
 
 
@@ -132,16 +112,20 @@ profileEditButton.addEventListener('click', function() {
    openPopup(editPopup);
 });
 
+// Функция для закрытия попапов
+function closePopup(popupElement) {
+   popupElement.classList.remove('popup_opened');
+}
 
-// Закрытие попапов
-function closePopup(args) {
+// Закрытие попапов, нажатием на иконку "крестик"
+function closePopupByClick(args) {
    const closeButton = args.target;
    const popupElement = closeButton.closest('.popup');
-   popupElement.classList.remove('popup_opened');
+   closePopup(popupElement);
 };
 
 closePopupButtons.forEach((closePopupButton) => {
-   closePopupButton.addEventListener('click', closePopup);
+   closePopupButton.addEventListener('click', closePopupByClick);
 });
 
 
@@ -158,7 +142,7 @@ function prepareCard(evt) {
    elementNameInput.value = null;
    elementLinkInput.value = null;
 
-   addPopup.classList.remove('popup_opened');
+   closePopup(addPopup);
 };
 
 
