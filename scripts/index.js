6411eb7c41467e89ea81987059ce32d0655b05d2
Vyacheslav Kostolomov <vyacheslav.kostolomov@yandex.ru>
2022-06-30 21:16:17 +0500

    fix: delete one of config selector

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,6 @@ export const config = {
    submitButtonSelector: '.popup__save-button',
    inactiveButtonClass: 'popup__save-button_disabled',
    inputErrorClass: 'popup__input_type_error',
-   errorElement: 'popup__input-error',
    errorClass: 'popup__input-error_visible'
 };
 
